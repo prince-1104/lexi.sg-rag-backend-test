50d00f9c107d996db9b9d1661d1b486eac40c19f
prince <115145305+prince-1104@users.noreply.github.com>
2025-07-11 17:47:36 +0530

    setup cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.rag_pipeline import handle_query
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"],
+)
 
 class QueryRequest(BaseModel):
     query: str
